be519ef93dced5f91d8f3065519c434bcf96d256
lfchain <lfucheng@yahoo.com>
2021-05-10 00:37:59 -0400

    finished local dev

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,4 +1,4 @@
 # Stores all configuration values
 import os
 SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
-MONGODB_HOST = "mongodb://localhost:27017/bill_tracker"
+MONGODB_HOST = os.getenv("MONGODB_HOST")
